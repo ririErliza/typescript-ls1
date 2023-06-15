0c7df95956edb2832732cb65aec64df66c4a2bac
ririErliza <95483807+ririErliza@users.noreply.github.com>
2023-06-15 21:38:18 +0200

    TS Config

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+let username = 'Jenny'
+console.log(username)
+
+// let a = 12
+// let b = '6'
+// let c = 2
+
+
+let a: number = 12
+let b: number = 6
+let c: number = 2
+
+console.log(a/b)
+
+console.log(c*b)
+
+
+let d: number = 1
+let e: number = 3
+
+console.log(d*e)
+

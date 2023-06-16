118a5977effed18054893d2f4b72ccfc69584317
ririErliza <95483807+ririErliza@users.noreply.github.com>
2023-06-16 11:17:53 +0200

    lesson 1 finished

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,1 @@
 "use strict";
-let username = 'Jenny';
-console.log(username);
-// let a = 12
-// let b = '6'
-// let c = 2
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
-let d = 1;
-let e = 3;
-console.log(d * e);
